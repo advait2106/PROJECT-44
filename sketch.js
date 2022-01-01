a360bf7097c76cb969fee857657d74199927d5e0
advait2106 <76954638+advait2106@users.noreply.github.com>
2022-01-01 22:24:36 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,33 @@
+var girl,girlIsJumping, girlsJumpingHigh
+var butterfly, butterflyIsCaught;
+
+
+function setup() {
+  
+  createCanvas(800,400);
+  createSprite(400, 200, 50, 50);
+  jumping = x 
+  girl.xPosition= -2;
+  
+  butterfly.color ("Blue")
+  mouseY.isPressed=girlIsJumping
+  UP_ARROW.isPressed=girlsJumpingHigh 
+}
+
+function draw() {
+  background("RED");  
+  gameState = (0);
+  getCount - (butterflyIsCaught=1)
+   gameStart = (0);
+  gameOver = butterflyIsCaught
+   play - gameStart
+restart. gameOver
+  button.createButton("Play")
+  button.createButton("Restart")
+
+
+  frameCount === 100
+
+
+  drawSprites();
+}
